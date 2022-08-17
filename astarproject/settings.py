c871d700f7aaa52560287029a4897442d3fc09fe
ahmedessam2008 <ahmessam@yahoo.com>
2022-08-17 08:55:29 +0200

    deploy to heroku

--- astarproject/settings.py
+++ astarproject/settings.py
@@ -14,8 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*^dywb57-clem0a^(n42@rjr#^0@17!80l!shdfbgo%c)f(%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1:8000','astar-ecommerce.herokuapp.com']
 
 
